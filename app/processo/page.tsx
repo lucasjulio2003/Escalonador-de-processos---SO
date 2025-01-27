f258682d3a5f437fa5052f34e5e3f14759a88154
Lucas Júlio <lucasjuliosouza.06@gmail.com>
2025-01-27 15:54:17 -0300

    aprendendo a criar componente

---
+++ app/processo/page.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.scss';
+
+export default function Processo() {
+    return (
+        // Div com classe Processo
+        <main className={styles.Processo}>
+            <h1>Processo</h1>
+        </main>
+    );
+}
