d34fe35a558f483ffc8f37878cd3c4d94f44a9fb
Lucas Júlio <lucasjuliosouza.06@gmail.com>
2025-02-01 21:54:05 -0300

    adiciona restrições de valor mínimo nos campos de entrada do formulário de processos

--- app/page.tsx
+++ app/page.tsx
@@ -45,6 +45,7 @@ export default function Home() {
 
             <input
               type="number"
+              min={1}
               value={quantum}
               onChange={(e) => setQuantum(Number(e.target.value))}
               className="p-2 border rounded text-black w-20"
@@ -60,6 +61,7 @@ export default function Home() {
             <label className="block text-white text-sm">Sobrecarga:</label>
             <input
               type="number"
+              min={1}
               value={overhead}
               onChange={(e) => setOverhead(Number(e.target.value))}
               className="p-2 border rounded text-black w-20"
